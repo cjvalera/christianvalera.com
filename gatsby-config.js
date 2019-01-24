26d37aa94a2d73a3ad4ade49d2de49e7ee65bdcd
Christian Valera <cjvalera@hotmail.com>
2019-01-24 02:17:27 -0800

    Updated Gatsby
    Added bronco express policy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,33 @@
 module.exports = {
   siteMetadata: {
-    title: 'Christian Valera',
+    title: `Christian Valera`,
+    author: `@cjvalera`
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
+        pathToConfigModule: `src/utils/typography.js`
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'cjvalera-site',
-        short_name: 'cjvalera',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
-      },
+        name: "cjvalera-site",
+        short_name: "cjvalera",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/favicon.png" // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-124564796-1',
+        trackingId: "UA-124564796-1",
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
@@ -34,9 +35,11 @@ module.exports = {
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-      },
+        exclude: ["/preview/**", "/do-not-track/me/too/"]
+      }
     },
-    'gatsby-plugin-offline',
-  ],
-}
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    "gatsby-plugin-offline"
+  ]
+};
---
+++ src/pages/bronco-express-policy.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+import Layout from "../components/layout";
+
+const BroncoExpressPrivacyPolicy = () => (
+  <Layout>
+    <div>
+      <h2>Bronco Express Privacy Policy</h2>
+      <p>
+        This policy applies to all information collected or submitted on Bronco
+        Express app for iOS.
+      </p>
+
+      <h3>Information we collect</h3>
+      <p>We store information about your initial route in local device only.</p>
+
+      <h3>Technical basics</h3>
+      <p>
+        If you enable notifications, you will receive local notification.
+        We never use notifications for marketing.
+      </p>
+
+      <h3>Bronco Shuttle</h3>
+      <p>
+        We use the API used on{" "}
+        <a href="https://www.broncoshuttle.com/">Bronco Shuttle</a> which is
+        operated and owned Parking & Transportation of Cal Poly Pomona.
+      </p>
+      <h3>Ads and analytics</h3>
+      <p>No personal data is used to target Bronco Express ads.</p>
+      <h3>Information usage</h3>
+      <p>
+        We use the information we collect to operate and improve our website,
+        apps, and customer support.
+      </p>
+      <p>
+        We do not share personal information with outside parties except to the
+        extent necessary to accomplish Bronco Express’ functionality. We may
+        share anonymous, aggregate statistics with outside parties.
+      </p>
+      <p>
+        We may disclose your information in response to subpoenas, court orders,
+        or other legal requirements; to exercise our legal rights or defend
+        against legal claims; to investigate, prevent, or take action regarding
+        illegal activities, suspected fraud or abuse, violations of our
+        policies; or to protect our rights and property.
+      </p>
+      <p>
+        In the future, we may sell to, buy, merge with, or partner with other
+        businesses. In such transactions, user information may be among the
+        transferred assets.
+      </p>
+      <h3>Security</h3>
+      <p>
+        We use the same API used to get real time status of bus schedules.
+        There’s no guarantee on 100% security as these API can change.
+      </p>
+      <h3>Third-party links and content</h3>
+      <p>
+        Bronco Express displays links and content from third-party sites. These
+        have their own independent privacy policies, and we have no
+        responsibility or liability for their content or activities.
+      </p>
+      <h3>California Online Privacy Protection Act Compliance</h3>
+      <p>
+        We comply with the California Online Privacy Protection Act. We
+        therefore will not distribute your personal information to outside
+        parties without your consent.
+      </p>
+      <h3>Children’s Online Privacy Protection Act Compliance</h3>
+      <p>
+        We never collect or maintain information at our website from those we
+        actually know are under 13, and no part of our website is structured to
+        attract anyone under 13.
+      </p>
+      <h3>Information for European Union Customers</h3>
+      <p>
+        By using Bronco Express and providing your information, you authorize us
+        to collect, use, and store your information outside of the European
+        Union.
+      </p>
+      <h3>International Transfers of Information</h3>
+      <p>
+        Information may be processed, stored, and used outside of the country in
+        which you are located. Data privacy laws vary across jurisdictions, and
+        different laws may be applicable to your data depending on where it is
+        processed, stored, or used.
+      </p>
+      <h3>Your Consent</h3>
+      <p>By using our site or apps, you consent to our privacy policy.</p>
+      <h3>Contacting Us</h3>
+      <p>
+        If you have questions regarding this privacy policy, you may email
+        <a href="mailto:cjvalera@hotmail.com">cjvalera@hotmail.com</a>.
+      </p>
+    </div>
+  </Layout>
+);
+
+export default BroncoExpressPrivacyPolicy;
