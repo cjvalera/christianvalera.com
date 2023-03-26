e1eaaa88d4695bc52783b3936e50b07b64db8edd
Christian Valera <cjvalera@hotmail.com>
2023-03-26 03:03:39 -0700

    add image viewer

--- src/components/social.js
+++ src/components/social.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import styled from "styled-components";
 import { Mail, Github, Linkedin } from "@styled-icons/feather";
+import ImageViewer from "react-simple-image-viewer";
 
 const Wrapper = styled.div`
   display: flex;
@@ -59,14 +60,42 @@ const SocialIcons = styled.a`
 `;
 
 const Social = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const images = ["https://bit.ly/3FRVWfW"];
+
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
+
   return (
     <div>
       <Container>
         <Avatar
           className="bounce-in-top"
           src="https://avatars3.githubusercontent.com/u/8040772"
-          alt="Me @ Disney"
+          alt="Hawaii"
+          onClick={() => openImageViewer(0)}
         />
+
+        {isViewerOpen && (
+          <ImageViewer
+            src={images}
+            currentIndex={currentImage}
+            disableScroll={false}
+            closeOnClickOutside={true}
+            onClose={closeImageViewer}
+            backgroundStyle={{
+              backgroundColor: "rgba(0,0,0,0.9)",
+            }}
+          />
+        )}
         <ResumeButton
           className="flicker-in-1"
           href="https://github.com/cjvalera/resume/raw/master/resume.pdf"
